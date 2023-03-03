f9e77e0eef12f2cd0e755a730cbeab4d8e45a50b
SnigetMain <v.gontar@uniyar.ac.ru>
2023-03-03 21:47:00 +0300

    Добавил камеру

--- main/Game.cpp
+++ main/Game.cpp
@@ -2,6 +2,8 @@
 #include <SFML/System/Clock.hpp>
 #include <iostream>
 #include<Windows.h>
+#include "Maps.h";
+#include "View.h";
 
 using namespace sf;
 using namespace std;
@@ -11,6 +13,7 @@ int SCREENY = 600;
 
 class Player : public sf::Sprite {
 public:
+    int x = 50, y = 50;
     sf::Image image;
     sf::Texture texture;
     int original_scale_x;
@@ -26,13 +29,14 @@ public:
         texture.loadFromImage(image);
         this->setTexture(texture);
         this->setTextureRect(sf::IntRect(0, 0, 32, 32));
-        this->setPosition(10, 10);
+        this->setPosition(x, y);
         sf::Vector2f scale = this->getScale();
         original_scale_x = scale.x;
         original_scale_y = scale.y;
-        this->setScale(original_scale_x * SCREENX * 0.005,
-            original_scale_y * SCREENY * 0.0065);
+        /*this->setScale(original_scale_x * SCREENX * 0.005,
+            original_scale_y * SCREENY * 0.0065);*/
     }
+
 
 
     void update(float time) {
@@ -122,15 +126,48 @@ public:
                 }
             }
         }
+
+        //y = this->getPosition().y;
+        //x = this->getPosition().x;
+        getplayercoordinateforview(this->getPosition().x, this->getPosition().y);
     }
 };
+
+
+void GenerateMap(Sprite& s_map,RenderWindow& window) {
+    for (int i = 0; i < HEIGHT_MAP; i++)
+        for (int j = 0; j < WIDTH_MAP; j++)
+        {
+            if (TileMap[i][j] == ' ')  s_map.setTextureRect(IntRect(64, 224, 32, 32)); //если встретили символ пробел, то рисуем 1й квадратик
+            if (TileMap[i][j] == 's')  s_map.setTextureRect(IntRect(160, 160, 32, 32));//если встретили символ s, то рисуем 2й квадратик
+            if ((TileMap[i][j] == '0')) s_map.setTextureRect(IntRect(160, 160, 32, 32));//если встретили символ 0, то рисуем 3й квадратик
+
+            s_map.setPosition(j * 32, i * 32);//по сути раскидывает квадратики, превращая в карту. то есть задает каждому из них позицию. если убрать, то вся карта нарисуется в одном квадрате 32*32 и мы увидим один квадрат
+
+            /*s_map.setScale(SCREENX * 0.002,
+                SCREENY * 0.0035);*/
+
+            window.draw(s_map);//рисуем квадратики на экран
+        }
+
+}
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(SCREENX, SCREENY), "GameProject");
-    // sf::Style::Fullscreen
-    // sf::RenderWindow window(sf::VideoMode(800, 600), "GameProject");
-    // sf::CircleShape shape(100.f);
+    view.reset(FloatRect(0, 0, SCREENX/4, SCREENY/4));
+
+
+    Image map_image;//объект изображения для карты
+    map_image.loadFromFile("Assets/Kings and Pigs/Sprites/14-TileSets/Terrain (32x32).png");//загружаем файл для карты
+    Texture map;//текстура карты
+    map.loadFromImage(map_image);//заряжаем текстуру картинкой
+    Sprite s_map;//создаём спрайт для карты
+    s_map.setTexture(map);//заливаем текстуру спрайтом
+
+
+
+
 
     sf::Clock clock;
     Player player;
@@ -148,19 +185,17 @@ int main()
                 // player.setScale(player.original_scale_x * event.size.width * 0.01,
                                 // player.original_scale_y * event.size.height * 0.01);
             }
-        }
-
-        /*if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
-            window.close();
-        }
-        sf::Int32 time_to_sleep = int(1.f / 60) - time;
-        if (time_to_sleep > 0) {
-            sf::sleep(sf::microseconds(time_to_sleep));
-        }*/
+        } 
 
 
         player.update(time);
-        window.clear();
+
+        window.setView(view); // обновление камеры
+
+
+        window.clear(Color(128,106,89));
+
+        GenerateMap(s_map,window); // Генерация карты
         window.draw(player);
         window.display();
     }
