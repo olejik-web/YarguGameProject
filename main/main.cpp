12c7505e58cd80fb44b25ea500867541c242dd5b
olejik-web <eremuchew.gelo@gmail.com>
2023-03-03 18:58:24 +0300

    Добавил счетчик FPS

--- main/main.cpp
+++ main/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/System/Clock.hpp>
-
+#include <sstream>
+#include <vector>
+#include <stack>
 #if defined _WIN32 || defined _WIN64
 
 #include <Windows.h>
@@ -13,63 +15,129 @@ int SCREENY = GetSystemMetrics(SM_CYSCREEN);
 
 using namespace std;
 
-class Player : public sf::Sprite {
-    public:
-        sf::Image image;
-        sf::Texture texture;
-        int original_scale_x;
-        int original_scale_y;
-        Player() {
-            image.loadFromFile("../Assets/BlueWitch/run.png");
-            texture.loadFromImage(image);
-            this->setTexture(texture);
-            this->setTextureRect(sf::IntRect(0, 0, 31, 32));
-            this->setPosition(10, 10);
-            sf::Vector2f scale = this->getScale();
-            original_scale_x = scale.x;
-            original_scale_y = scale.y;
-            this->setScale(original_scale_x * SCREENX * 0.005,
-                                original_scale_y * SCREENY * 0.0065);
-        }
+float TIME;
+
+class FPS
+{
+public:
+    sf::Font font;
+    sf::Text text;
+	FPS() : mFrame(0), mFps(0) {
+        font.loadFromFile("DoomFont.ttf");
+        text.setFont(font);
+        text.setCharacterSize(34);
+        text.setFillColor(sf::Color::Red);
+        text.setPosition(SCREENX - 120, 30.f);
+	}
+	const unsigned int getFPS() const { return mFps; }
+
+private:
+	unsigned int mFrame;
+	unsigned int mFps;
+	sf::Clock mClock;
+
+public:
+	void update()
+	{
+		if(mClock.getElapsedTime().asSeconds() >= 1.f)
+		{
+			mFps = mFrame;
+			mFrame = 0;
+			mClock.restart();
+		}
+		++mFrame;
+        ostringstream out_fps;
+		out_fps << this->getFPS();
+        this->text.setString("FPS: " + out_fps.str());
+	}
 };
+
+class Player {
+    public:
+    float x, y, w, h, dx, dy, speed; //координаты игрока х и у, высота ширина, ускорение (по х и по у), сама скорость
+	vector<bool>dirs; //направление (direction) движения игрока
+	sf::String File; //файл с расширением
+	sf::Image image;//сфмл изображение
+	sf::Texture texture;//сфмл текстура
+	sf::Sprite sprite;//сфмл спрайт
+	sf::Vector2f vec;
+	Player(sf::String F, float X, float Y, float W, float H){  //Конструктор с параметрами(формальными) для класса Player. При создании объекта класса мы будем задавать имя файла, координату Х и У, ширину и высоту
+	    speed = 0.8f;
+		File = F;//имя файла+расширение
+		w = W; h = H;//высота и ширина
+		image.loadFromFile(File);
+		texture.loadFromImage(image);//закидываем наше изображение в текстуру
+		sprite.setTexture(texture);//заливаем спрайт текстурой
+		x = X; y = Y;//координата появления спрайта
+		sprite.setTextureRect(sf::IntRect(0, 0, w, h));  //IntRect - приведение типов
+		sf::Vector2f scale = sprite.getScale();
+        sprite.setScale(scale.x * SCREENX * 0.005, scale.y * SCREENY * 0.0065);
+        vec.x = 0;
+        vec.y = 0;
+        dirs = vector<bool>(4, false);
+	}
+	void update(float time) // функция "оживления" объекта класса. update - обновление. принимает в себя время SFML , вследствие чего работает бесконечно, давая персонажу движение.
+	{
+        if (dirs[0] != dirs[1]) {
+            if (dirs[0]) {vec.x = -speed;}
+            if (dirs[1]) {vec.x = speed;}
+        }
+        if (dirs[2] != dirs[3]) {
+            if (dirs[2]) {vec.y = -speed;}
+            if (dirs[3]) {vec.y = speed;}
+        }
+        sprite.move(vec);
+        vec.x = 0;
+        vec.y = 0;
+        dirs = vector<bool>(4, false);
+	}
+};
+
+FPS GameFPS;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(SCREENX, SCREENY), "GameProject", sf::Style::Fullscreen);
-    // sf::Style::Fullscreen
-    // sf::RenderWindow window(sf::VideoMode(800, 600), "GameProject");
-    // sf::CircleShape shape(100.f);
-    Player player;
+    sf::RenderWindow window(sf::VideoMode(SCREENX, SCREENY), "GameProject");
+    Player player("../Assets/BlueWitch/run.png", 10.0f, 10.0f, 31.0f, 32.0f);
     while (window.isOpen())
     {
         sf::Clock clock;
-        float time = clock.getElapsedTime().asMicroseconds();
+        TIME = clock.getElapsedTime().asMicroseconds();
         clock.restart();
-        time /= 1500;
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
-            if (event.type == sf::Event::Resized) {
-                // player.setScale(player.original_scale_x * event.size.width * 0.01,
-                                // player.original_scale_y * event.size.height * 0.01);
-            }
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-            player.move(-300.0*time, 0);
+        stack<char>st;
+		if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || (sf::Keyboard::isKeyPressed(sf::Keyboard::A)))) {
+            player.dirs[0] = true;
+            st.push('l');
+		}
+		if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || (sf::Keyboard::isKeyPressed(sf::Keyboard::D)))) {
+            player.dirs[1] = true;
+            st.push('r');
+		}
+		if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || (sf::Keyboard::isKeyPressed(sf::Keyboard::W)))) {
+            player.dirs[2] = true;
+            st.push('u');
+		}
+		if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || (sf::Keyboard::isKeyPressed(sf::Keyboard::S)))) { //если нажата клавиша стрелка влево или англ буква А
+            player.dirs[3] = true;
+            st.push('d');
+		}
+		// cout << player.dirs[0] << " " << player.dirs[1] << " " << player.dirs[2] << " " << player.dirs[3] << "\n";
+        /* while (!st.empty()) {
+            cout << st.top() << " ";
+            st.pop();
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-            player.move(300.0*time, 0);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-            player.move(0, -300.0*time);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-            player.move(0, 300.0*time);
-        }
+        cout << "\n";*/
+        player.update(TIME);
+        GameFPS.update();
         window.clear();
-        window.draw(player);
+        window.draw(player.sprite);
+        window.draw(GameFPS.text);
         window.display();
     }
     return 0;
